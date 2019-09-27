89c15b2d0219211944e6b7977fc0a9c6bf3c1e5e
avijit dhar <avijit.dhar@edifixio.com>
2019-09-27 16:34:09 +0530

    fix backend file issue

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraforms-remote-state-bucket"
-    key    = "state-file-app"
+    bucket = "terraform-edfx-backup-state"
+    key = "state-file"
     region = "us-east-1"
   }
-
+}
