4971b4a7c79c3ab2bf32641c2b671a087759d0a3
avijit dhar <avijit.dhar@edifixio.com>
2019-09-29 15:22:06 +0530

    add valiable

--- provider.tf
+++ provider.tf
@@ -1,4 +1,8 @@
-provider "aws" {
-  region = var.AWS_REGION
-}
+variable "access_key" {}
+variable "secret_key" {}
 
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "var.AWS_REGION"
+}
