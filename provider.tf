0ec4434fc8a032d5aafe48a43a6e80a732ffd9fe
avijit dhar <avijit.dhar@edifixio.com>
2019-09-29 16:21:39 +0530

    fixed vars file

--- provider.tf
+++ provider.tf
@@ -4,5 +4,5 @@ variable "secret_key" {}
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-  region     = "var.AWS_REGION"
+  region     = "${var.AWS_REGION}"
 }
