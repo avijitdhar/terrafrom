c6d4766ed3d7f45ad609e6ccb672434c61d35c26
avijit dhar <avijit.dhar@edifixio.com>
2019-09-29 16:53:25 +0530

    fix issue for credential

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ variable "access_key" {}
 variable "secret_key" {}
 
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+  access_key = "${var.AWS_ACCESS_KEY_ID}"
+  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
   region     = "${var.AWS_REGION}"
 }
